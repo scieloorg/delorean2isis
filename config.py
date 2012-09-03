678848b52e5d7de2be2c5563d66a60292d082706
Jamil Atta Junior <atta.jamil@gmail.com>
2012-09-03 15:40:30 -0300

    Add fst templates

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #coding:utf-8
 
-DELOREAN_URL = 'http://DOMAIN:PORT/generate/'
+DELOREAN_URL = 'http://localhost:6543/generate/'
 ISIS_PATH = ""
 DATABASES = ['title', "issue"]
 DATABASE_FST = {'title': 'tit_issn', 'issue': 'issue'}
