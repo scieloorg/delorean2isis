37d87990ce35be4e9d94f1b93e9914acfccfdd89
Jamil Atta Junior <atta.jamil@gmail.com>
2012-09-03 14:25:11 -0300

    Script to generate Title, Issue and Section for processing SciELO

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#coding:utf-8
+
+DELOREAN_URL = 'http://DOMAIN:PORT/generate/'
+ISIS_PATH = ""
+DATABASES = ['title', "issue"]
+DATABASE_FST = {'title': 'tit_issn', 'issue': 'issue'}
