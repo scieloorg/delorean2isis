eefb6fd16ace56985be24eea0d3c0b6335d48063
Jamil Atta Junior <atta.jamil@gmail.com>
2013-01-07 10:36:22 -0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,5 @@
 API_URL = 'http://localhost:8000/api/v1/'
 DELOREAN_URL = 'http://localhost:6543/generate/'
 ISIS_PATH = ""
-#DATABASES = ['title', "issue"]
 DATABASES = ["title", "issue"]
 DATABASE_FST = {'title': 'tit_issn', 'issue': 'issue'}
